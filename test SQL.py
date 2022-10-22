39fc70f4db4371ed7a77b54413993513fc51e993
Ludwik <ludwikpapaj@gmail.com>
2022-10-22 14:41:29 +0200

    4

--- test SQL.py
+++ test SQL.py
@@ -1,4 +1,5 @@
 import mysql.connector
+from datetime import datetime
 
 db = mysql.connector.connect(
     host = "localhost",
@@ -15,9 +16,15 @@ mycursor = db.cursor()
 #db.commit()
 
 #mycursor.execute("DESCRIBE Person")
-mycursor.execute("SELECT * FROM Person")
+#mycursor.execute("SELECT * FROM Person")
 
+#mycursor.execute("CREATE TABLE Test (name varchar(50) NOT NULL, created datetime NOT NULL, gender ENUM('M','F','O') NOT NULL, id int PRIMARY KEY NOT NULL AUTO_INCREMENT)")
+#mycursor.execute("INSERT INTO Test (name, created, gender) VALUES (%s,%s,%s)", ("TIM", datetime.now(), "M"))
+#db.commit()
 
+#mycursor.execute("SELECT id, name FROM Test WHERE gender = 'M' ORDER BY id DESC")
+#mycursor.execute("ALTER TABLE Test ADD COLUMN food VARCHAR(50) NOT NULL")
+#mycursor.execute("ALTER TABLE name first_name VARCHAR(50)")
 for x in mycursor:
-    print(x)
+   print(x)
 
