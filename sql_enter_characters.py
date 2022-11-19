e0c2f9ad2b26c7c9a911ac516df7e634eb465df0
Ludwik <ludwikpapaj@gmail.com>
2022-11-19 15:08:12 +0100

    sql select 1

--- sql_enter_characters.py
+++ sql_enter_characters.py
@@ -1,6 +1,7 @@
 import mysql.connector
 from datetime import datetime
 import WTcredentials
+import csv
 
 db = mysql.connector.connect(
     host = WTcredentials.host,
@@ -10,24 +11,49 @@ db = mysql.connector.connect(
 
 mycursor = db.cursor()
 
-sql_query = "INSERT INTO mapping_to_type VALUES (%s, %s)"
-sql_values = [
-    (0, 'NULL'),
+#sql_query = "INSERT INTO mapping_to_type VALUES (%s)"
+#sql_query = ("SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS"
+#"WHERE TABLE_SCHEMA=(%s) AND TABLE_NAME=(%s);")
+#sql_values = ('storytool_test', 'character')
+#mycursor.execute(sql_query, sql_values)
 
-]
 
-mycursor.execute(sql_query, sql_values)
-db.commit()
-
-print(mycursor.rowcount, "was inserted.")
+sql_query  =  """
+SELECT COLUMN_NAME
+FROM `INFORMATION_SCHEMA`.`COLUMNS`
+WHERE `TABLE_SCHEMA` = 'storytool_test'
+AND `TABLE_NAME` = 'character'
+"""
+mycursor.execute(sql_query)
+for col in mycursor.fetchall():
+    print(col)
+#WORKS!!!!!!!!!!"
 
 
 
 def enter_characters(csvfile):
     pass
 
-def map_csv(csvfile):
-    pass
+def read_csv(csvfile):
+    with open(csvfile, 'r', encoding='utf-8') as file:
+        content = csv.reader(file)
+        headings = next(content)
+        output = []
+        for row in content:
+               output.append(row)
+    return(headings, output)
+
+lines_to_verify = read_csv("testADD_CHARACTERS.csv")
+print(lines_to_verify[0])
+print(*lines_to_verify[1])
+
+"""
+SELECT `COLUMN_NAME` 
+FROM `INFORMATION_SCHEMA`.`COLUMNS` 
+WHERE `TABLE_SCHEMA`='yourdatabasename' 
+    AND `TABLE_NAME`='yourtablename';
+"""
+WTcredentials.database
 
 '''
 INSERT INTO `storytool_test`.`character`
