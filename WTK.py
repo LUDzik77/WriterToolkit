f501b7cfafda32b07e00b4ee4282e80a804f39d7
Ludwik <ludwikpapaj@gmail.com>
2022-12-18 16:07:19 +0100

    tests_+_books

--- WTK.py
+++ WTK.py
@@ -15,8 +15,8 @@ db = mysql.connector.connect(
 mycursor = db.cursor()
 
 #temp test paths
-CHARACTERS_CSV = "testADD_CHARACTERS.csv"
-SCENES_CSV = "testADD_CHARACTERS.csv"
+CHARACTERS_CSV = "ADD_CHARACTERS.csv"
+SCENES_CSV = "ADD_CHARACTERS.csv"
 CHAPTERS_CSV = "ADD_CHAPTERS.csv"
 BOOKS_CSV = "ADD_BOOKS.csv"
           
@@ -60,7 +60,9 @@ def pair_actions(headings, lines):
 def execute_commit(sql_query, sql_values):
     mycursor.execute(sql_query, sql_values)
     db.commit()
-    print(f"entered {sql_values}")
+    #isApple = True if fruit == 'Apple' else False
+    #print(f"Entered or modified: {sql_values}")
+    print(f"Removed: {sql_values}") if len(sql_values) == 1 else print(f"Entered: {sql_values}")
     
 def enter_character(line_with_headings):
     sql_query = "INSERT INTO `storytool_test`.`character`"\
@@ -73,9 +75,9 @@ def enter_character(line_with_headings):
 #enter_character({'ACTION': 'ADD', 'character_id': '', 'first_name': 'Łukasz', 'family_name': 'Sikora','nickname': 'Szeryf',\
 # 'principal': '1', 'description': 'tall, medium hair', 'gender': '1', 'skill': 'bjj', 'idea': 'centrism', 'saying': '', 'narrative': ''})
 
-#work in progress DELATE + INSERT  instead of UPDATE
+#work in progress DELETE + INSERT  instead of UPDATE
 def modify_character(line_with_headings):
-    sql_query1 ="DELETE FROM `storytool_test`.`character`WHERE character_id=(%s);";   
+    sql_query1 = "DELETE FROM `storytool_test`.`character`WHERE character_id=(%s);"   
     sql_query2 = "INSERT INTO `storytool_test`.`character`"\
         +list_to_str_with_escape_characters(get_columns_names_without('character'))\
         + " VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
@@ -84,13 +86,8 @@ def modify_character(line_with_headings):
         sql_values.append(line_with_headings[heading])
     execute_commit(sql_query1, tuple(line_with_headings['character_id']))
     execute_commit(sql_query2, tuple(sql_values))
-modify_character({'ACTION': 'M', 'character_id': '1', 'first_name': 'ŁukaszCHANGED', 'family_name': 'SikoraCHANGED','nickname': 'SzeryfCHANGED',\
-                'principal': '1', 'description': 'tall, medium hair', 'gender': '1', 'skill': 'bjj', 'idea': 'centrism', 'saying': '', 'narrative': ''})
-
-#UPDATE Customers
-#SET ContactName = 'Alfred Schmidt', City= 'Frankfurt'
-#WHERE CustomerID = 1;
-
+#modify_character({'ACTION': 'M', 'character_id': '1', 'first_name': 'ŁukaszCHANGED', 'family_name': 'SikoraCHANGED','nickname': 'SzeryfCHANGED',\
+                #'principal': '1', 'description': 'tall, medium hair', 'gender': '1', 'skill': 'bjj', 'idea': 'centrism', 'saying': '', 'narrative': ''})
 
 def enter_book(line_with_headings):
     sql_query = "INSERT INTO `storytool_test`.`book`"\
@@ -101,6 +98,17 @@ def enter_book(line_with_headings):
         sql_values.append(line_with_headings[heading])
     execute_commit(sql_query, tuple(sql_values))
 #enter_book({'ACTION': 'ADD', 'name': 'Dzieciaki', 'completed':'0', 'description':'sci-fi  novel', 'narrative':'' })
+
+def modify_book(line_with_headings):
+    sql_query1 = "DELETE FROM `storytool_test`.`book`WHERE name=(%s);"  
+    sql_query2 = "INSERT INTO `storytool_test`.`book`"\
+        +list_to_str_with_escape_characters(get_columns_names_without('book'))\
+        + " VALUES (%s, %s, %s, %s)"
+    sql_values = list()
+    for heading in get_columns_names_without('book'):
+        sql_values.append(line_with_headings[heading])
+    execute_commit(sql_query1, tuple(line_with_headings['name']))
+    execute_commit(sql_query2, tuple(sql_values))
 
 def enter_chapter(line_with_headings):
     sql_query = "INSERT INTO `storytool_test`.`chapter`"\
@@ -113,6 +121,7 @@ def enter_chapter(line_with_headings):
 
 def map_and_execute(file, func_add, func_modify):
     for line in pair_actions(*file):
+        print(line)
         if line['ACTION'] in ("ADD", "add", "a", "A", "+"): func_add(line)
         elif line['ACTION'] in ("MODIFY", "modify", "m", "M"): func_modify(line) 
         else: print("Only ADD or MODIFY as action are allowed")   
@@ -130,8 +139,8 @@ def truncate_table(table_name_with_escape_symbols):
     print(f"{table_name_with_escape_symbols} TABLE was truncated ") 
 
 def characters():
-    truncate_table("`character`")
-    map_and_execute(ADD_OR_MODIFY_CHARACTERS, enter_character, enter_character)     
+    #truncate_table("`character`")
+    map_and_execute(ADD_OR_MODIFY_CHARACTERS, enter_character, modify_character)     
     #truncate_table("`character`")
 #characters()
 
@@ -142,14 +151,15 @@ def charactersID():
 #charactersID()
 
 def scenes():
+    #truncate_table("`scene`")
     map_and_execute(ADD_OR_MODIFY_SCENES, enter_scene, enter_scene)
-    truncate_table("`scene`")
+    #truncate_table("`scene`")
 
 def chapters():
     map_and_execute(ADD_OR_MODIFY_CHAPTERS, enter_chapter, enter_chapter)
     truncate_table("`chapter`")
 
 def books():
-    map_and_execute(ADD_OR_MODIFY_BOOKS, enter_book, enter_book)
+    map_and_execute(ADD_OR_MODIFY_BOOKS, enter_book, modify_book)
     truncate_table("`book`")
-
+books()
